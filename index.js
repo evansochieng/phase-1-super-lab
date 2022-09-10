58fb6d92b564ca31a4c79dc288ed5e38c0bd5a2e
Evans Ochieng <evansochieng121@gmail.com>
2022-09-10 15:36:49 +0300

    Completed using super as method and object

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your classes here
+
+// Parent class Tree
+class Tree {
+    constructor(species){
+        this.species = species;
+    }
+
+    static definition(){
+        return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
+    }
+}
+
+// child classes
+// class Deciduous
+class Deciduous extends Tree{
+    constructor(species, name){
+        super(species);
+        this.name = name;
+    }
+
+    // static method that adds to return value of parent method
+    static definition(){
+        return (
+            super.definition() + " " + "Deciduous trees shed their leaves annually."
+        );
+    }
+}
+
+// class Evergreen
+class Evergreen extends Tree {
+    constructor(species, name){
+        super(species);
+        this.name = name;
+    }
+
+    // static method that adds to return value of parent method
+    static definition(){
+        return (
+            super.definition() + " " + "Evergreens keep their leaves all year round."
+        );
+    }
+}
